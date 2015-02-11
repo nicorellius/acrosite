aea46432451338ce8d592dad082d385cf5c6409d
nicorellius <nicorellius@gmail.com>
2015-02-10 16:36:47 -0800

    removed constructions and fixed syntax and formatting on build_constructions

--- acros/apps/generator/build_construction.py
+++ acros/apps/generator/build_construction.py
@@ -1,18 +1,22 @@
-'''
-Created on Jan 18, 2015
-
-@author: phillipseitzer
-'''
+"""
+file        :   build_constructions.py
+date        :   2015-01-18
+module      :   generator
+classes     :
+description :   Build constructions for generating acrostics
+"""
 
 import random
 
-from .parts_of_speech import adj_to_noun_verb_adv, adj_to_noun, adj_adj_noun_pattern, all_nouns,adj_noun_verb_adv_conn_pattern
+from .parts_of_speech import adj_to_noun_verb_adv, adj_to_noun
+from .parts_of_speech import adj_adj_noun_pattern, all_nouns, adj_noun_verb_adv_conn_pattern
 
-from .build_filter import add_first_letter_filter,add_part_of_speech_filter,add_tag_filter
-from .build_filter import add_tag_list_filter,condense_tags_list,add_tag_OR_filter,add_pos_OR_filter
-from. build_filter import len_valid_words,len_valid_characters,clean_word
+from .build_filter import add_first_letter_filter, add_part_of_speech_filter, add_tag_filter
+from .build_filter import add_tag_list_filter, condense_tags_list, add_tag_OR_filter, add_pos_OR_filter
+from. build_filter import len_valid_words, len_valid_characters, clean_word
 
 from .models import Word
+
 
 def create_acrostic_filters(vert_word, word_list, theme_name, construction_type):
     acrostic_data = []
@@ -32,8 +36,10 @@ def create_acrostic_filters(vert_word, word_list, theme_name, construction_type)
             acrostic_data = my_name_adjectives(vert_word, word_list, False)
         elif construction_type == 4:
             acrostic_data = my_name_adv_adj(vert_word, word_list, False)
+
     elif theme_name == 'cute_animals':
         acrostic_data = cute_animals_theme(vert_word, word_list, construction_type)
+
     elif theme_name == 'music':
         if construction_type == 1:
             acrostic_data = instruments_making_music(vert_word, word_list)
@@ -44,9 +50,11 @@ def create_acrostic_filters(vert_word, word_list, theme_name, construction_type)
             
     return acrostic_data
 
+
 def my_name_adjectives(vert_word, word_list, is_positive):
     
     part_of_speech = 'A'
+
     if is_positive:
         tags = ['positive']
     else:
@@ -65,7 +73,6 @@ def my_name_adjectives(vert_word, word_list, is_positive):
     return [filters, part_of_speech, tags]
 
 
-
 def my_name_adv_adj(vert_word, word_list, is_positive):
 
     characters = list(vert_word)
@@ -73,8 +80,10 @@ def my_name_adv_adj(vert_word, word_list, is_positive):
     word_num = len_valid_words(word_list)
     
     part_of_speech = ''
+
     if is_positive:
         tags = ['positive']
+
     else:
         tags = ['negative']
     
@@ -97,6 +106,7 @@ def my_name_adv_adj(vert_word, word_list, is_positive):
     
     if part_of_speech == 'A':
         tags.append('to_person')
+
     elif part_of_speech == 'D':
         tags.append('precede_adjective')
                 
@@ -107,6 +117,7 @@ def my_name_adv_adj(vert_word, word_list, is_positive):
     add_part_of_speech_filter(filters, part_of_speech)
     
     return [filters, part_of_speech, tags]
+
 
 def cute_animals_theme(vert_word, word_list, construction_type):
     
@@ -121,6 +132,7 @@ def cute_animals_theme(vert_word, word_list, construction_type):
         parts_of_speech = adj_to_noun(clean_word(vert_word), True);
     elif construction_type == 5:
         parts_of_speech = all_nouns(clean_word(vert_word), True)
+
     characters = list(vert_word)
     word_num = len_valid_words(word_list)
        
@@ -128,11 +140,14 @@ def cute_animals_theme(vert_word, word_list, construction_type):
 
     add_first_letter_filter(filters, characters[len(word_list)])
     tags = []
+
     if word_num < len(parts_of_speech):
+
         part_of_speech = parts_of_speech[word_num]
         add_part_of_speech_filter(filters, parts_of_speech[word_num])
     
         tags = []
+
         if parts_of_speech[word_num] == 'E':
             tags.append('positive')
         elif parts_of_speech[word_num] == 'A':
@@ -145,11 +160,14 @@ def cute_animals_theme(vert_word, word_list, construction_type):
         elif parts_of_speech[word_num] == 'D':
             tags.append('follow_verb')
             tags.append('cute_animal_theme')
+
         add_tag_list_filter(filters, tags)
+
     else:
         part_of_speech = ''        
     
     return [filters, part_of_speech, condense_tags_list(tags)]
+
 
 def animals_jamming(vert_word, word_list):
     
@@ -189,6 +207,7 @@ def animals_jamming(vert_word, word_list):
             parts_of_speech.append('A')
             
         counter = 0
+
         while counter < word_length:
             parts_of_speech.append('NP')
             parts_of_speech.append('VP')
@@ -208,6 +227,7 @@ def animals_jamming(vert_word, word_list):
         tags = tags_list[word_num]
 
     if word_length == 10:
+
         if word_num == 0:
             part_of_speech = 'E'
             tags = []
@@ -240,6 +260,7 @@ def animals_jamming(vert_word, word_list):
             tags = ['musical_instrument']
                 
     if word_length == 9:
+
         if word_num == 0:
             part_of_speech = 'A'
             tags = ['positive','cute_animal_theme']
@@ -269,6 +290,7 @@ def animals_jamming(vert_word, word_list):
             tags = ['musical_instrument']
     
     if word_length == 8:
+
         if word_num == 0:
             part_of_speech = 'E'
             tags = []
@@ -293,8 +315,7 @@ def animals_jamming(vert_word, word_list):
         elif word_num == 7:
             part_of_speech = 'NP'
             tags = ['musical_instrument']
-    
-    
+
     if word_length == 7:
         if word_num == 0:
             part_of_speech = 'NP'
@@ -320,12 +341,13 @@ def animals_jamming(vert_word, word_list):
     
     # start with exclamation.
     if word_length > 3 and word_length < 7:
-        if word_length - word_num  == 6:
+
+        if word_length - word_num == 6:
             part_of_speech = 'E'
             tags = ['positive']
         elif word_length - word_num == 5:
             part_of_speech = 'A'
-            tags = ['positive','cute_animal_theme']
+            tags = ['positive', 'cute_animal_theme']
         elif word_length - word_num == 4:
             part_of_speech = 'NP'
             tags = ['cute_animal']
@@ -337,9 +359,10 @@ def animals_jamming(vert_word, word_list):
             tags = ['musical_instrument']
         elif word_length - word_num == 1:
             part_of_speech = 'D'
-            tags = ['follow_verb','positive']
+            tags = ['follow_verb', 'positive']
     
     if word_length == 3:
+
         if word_num == 0:
             part_of_speech = 'NP'
             tags = ['cute_animal']
@@ -351,6 +374,7 @@ def animals_jamming(vert_word, word_list):
             tags = ['musical_instrument']
 
     if word_length == 2:
+
         if word_num == 0:
             part_of_speech = 'NP'
             tags = ['cute_animal']
@@ -359,6 +383,7 @@ def animals_jamming(vert_word, word_list):
             tags = ['make_music']
 
     if word_length == 1:
+
         part_of_speech = 'NP'
         tags = ['cute_animal']
         
@@ -369,7 +394,6 @@ def animals_jamming(vert_word, word_list):
     return [filters, part_of_speech, tags]
 
 
-    
 def just_instruments(vert_word, word_list):
     
     filters = []
@@ -399,67 +423,143 @@ def instruments_making_music(vert_word, word_list):
         parts_of_speech = ['NP']
         tags = [['musical_instrument']]
     elif word_length == 2:
-        parts_of_speech = ['NP','VP']
-        tags = [['musical_instrument'],['make_music']]
+        parts_of_speech = ['NP', 'VP']
+        tags = [
+            ['musical_instrument'],
+            ['make_music']
+        ]
     elif word_length == 3:
-        parts_of_speech = ['NP','VP','D']
-        tags = [['musical_instrument'],['make_music'],['positive','follow_verb']]
+        parts_of_speech = ['NP', 'VP', 'D']
+        tags = [
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb']
+        ]
     elif word_length == 4:
-        parts_of_speech = ['A','NP','VP','D']
-        tags = [['positive','to_person'],['musical_instrument'],['make_music'],['positive','follow_verb']]
+        parts_of_speech = ['A', 'NP', 'VP', 'D']
+        tags = [
+            ['positive', 'to_person'],
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb']
+        ]
     elif word_length == 5:
-        parts_of_speech = ['NP','VP','C','NP','VP']
-        tags = [['musical_instrument'],['make_music'],[],['musical_instrument'],['make_music']]
+        parts_of_speech = ['NP', 'VP', 'C', 'NP', 'VP']
+        tags = [
+            ['musical_instrument'],
+            ['make_music'],
+            [],
+            ['musical_instrument'],
+            ['make_music']
+        ]
     elif word_length == 6:
-        parts_of_speech = ['NP','VP','C','NP','VP','D']
-        tags = [['musical_instrument'],['make_music'],[],['musical_instrument'],['make_music'],['positive','follow_verb']]
+        parts_of_speech = ['NP', 'VP', 'C', 'NP', 'VP', 'D']
+        tags = [
+            ['musical_instrument'],
+            ['make_music'],
+            [],
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb']
+        ]
     elif word_length == 7:
-        parts_of_speech = ['NP','VP','D','C','NP','VP','D']
-        tags = [['musical_instrument'],['make_music'],['positive','follow_verb'],[],['musical_instrument'],['make_music'],['positive','follow_verb']]
+        parts_of_speech = ['NP', 'VP', 'D', 'C', 'NP', 'VP', 'D']
+        tags = [
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb'],
+            [], ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb']
+        ]
     elif word_length == 8:
-        parts_of_speech = ['A','NP','VP','D','C','NP','VP','D']
-        tags = [['to_person','positive'],['musical_instrument'],['make_music'],['positive','follow_verb'],[],['musical_instrument'],['make_music'],['positive','follow_verb']]
+        parts_of_speech = ['A', 'NP', 'VP', 'D', 'C', 'NP', 'VP', 'D']
+        tags = [
+            ['to_person', 'positive'],
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb'],
+            [],
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb']
+        ]
     elif word_length == 9:
-        parts_of_speech = ['A','NP','VP','D','C','A','NP','VP','D']
-        tags = [['positive','to_person'],['musical_instrument'],['make_music'],['positive','follow_verb'],[],['to_person','positive'],['musical_instrument'],['make_music'],['positive','follow_verb']]
+        parts_of_speech = ['A', 'NP', 'VP', 'D', 'C', 'A', 'NP', 'VP', 'D']
+        tags = [
+            ['positive', 'to_person'],
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb'],
+            [],
+            ['to_person', 'positive'],
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb']
+        ]
     elif word_length == 10:
-        parts_of_speech = ['A','A','NP','VP','D','C','A','NP','VP','D']
-        tags = [['to_person','positive'],['to_person','positive'],['musical_instrument'],['make_music'],['positive','follow_verb'],[],['to_person','positive'],['musical_instrument'],['make_music'],['positive','follow_verb']]
+        parts_of_speech = ['A', 'A', 'NP', 'VP', 'D', 'C', 'A', 'NP', 'VP', 'D']
+        tags = [
+            ['to_person', 'positive'],
+            ['to_person', 'positive'],
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb'],
+            [],
+            ['to_person', 'positive'],
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb']
+        ]
     elif word_length == 11:
-        parts_of_speech = ['NP','VP','D','C','NP','VP','D','C','NP','VP','D']
-        tags = [['musical_instrument'],['make_music'],['positive','follow_verb'],[],['musical_instrument'],['make_music'],['positive','follow_verb'],[],['musical_instrument'],['make_music'],['positive','follow_verb']]
+        parts_of_speech = ['NP', 'VP', 'D', 'C', 'NP', 'VP', 'D', 'C', 'NP', 'VP', 'D']
+        tags = [
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb'],
+            [],
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb'],
+            [],
+            ['musical_instrument'],
+            ['make_music'],
+            ['positive', 'follow_verb']
+        ]
     else:
         if word_length % 4 == 0:
-            tags.append(['positive','to_person'])
+            tags.append(['positive', 'to_person'])
             parts_of_speech.append('A')
             
         elif word_length % 4 == 1:
-            tags.append(['positive','to_person'])
+            tags.append(['positive', 'to_person'])
             parts_of_speech.append('A')
             
-            tags.append(['positive','to_person'])
+            tags.append(['positive', 'to_person'])
             parts_of_speech.append('A')
 
         elif word_length % 4 == 2:
 
-            tags.append(['positive','to_person'])
+            tags.append(['positive', 'to_person'])
             parts_of_speech.append('A')
             
-            tags.append(['positive','to_person'])
+            tags.append(['positive', 'to_person'])
             parts_of_speech.append('A')
             
-            tags.append(['positive','to_person'])
+            tags.append(['positive', 'to_person'])
             parts_of_speech.append('A')
 
         counter = 0
+
         while counter < word_length:
+
             parts_of_speech.append('NP')
             parts_of_speech.append('VP')
             parts_of_speech.append('D')
                 
             tags.append(['musical_instrument'])
             tags.append(['make_music'])
-            tags.append(['positive','follow_verb'])
+            tags.append(['positive', 'follow_verb'])
             
             if counter + 3 < word_length:
                 parts_of_speech.append('C')
@@ -472,4 +572,3 @@ def instruments_making_music(vert_word, word_list):
     add_tag_list_filter(filters, tags[word_num])
     
     return [filters, parts_of_speech[word_num], condense_tags_list(tags[word_num])]
-    

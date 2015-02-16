91a4c45e72418c19b224e961a9dd54eb1801a9b7
nicorellius <nicorellius@gmail.com>
2015-02-16 09:06:34 -0800

    added twitter, facebook and google plus share bits and cloned development settings to production for deployment

--- acros/acros/settings/production.py
+++ acros/acros/settings/production.py
@@ -4,28 +4,58 @@ Django production settings for acrosite project.
 
 from .base import *
 
-# set to False for production
-DEBUG = False
+# set to `True` for development
+DEBUG = True
 
-# for databases, https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-# postgresql database setting
-#with open('/path/to/db_password.txt') as db_password:
-#    DB_PASSWORD = db_password.read().strip()
+# secret key in local settings
+with open('/home/dev/prv/acros/secret_key.txt') as secret_key:
+    SECRET_KEY = secret_key.read().strip()
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': '',
-#        'USER': '',
-#        'PASSWORD': DB_PASSWORD
-#        'HOST': '',
-#        'PORT': '',
-#    }
-#}
+# email smtp settings
+with open('/home/dev/prv/acros/email_password.txt') as email_password:
+    EMAIL_PASSWORD = email_password.read().strip()
+
+EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(SITE_ROOT, 'db.sqlite3'),
+    }
+}
+
+# email settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'acrostic.mail@gmail.com'
+# this password is meant to be over-ridden in `settings/<devs_name>.py`
+# eg, set EMAIL_HOST_PASSWORD = EMAIL_PASSWORD after reading password from file
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+
+
+"""
+For databases, https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+PostgreSQL database setting
+"""
+"""
+with open('/path/to/db_password.txt') as db_password:
+    DB_PASSWORD = db_password.read().strip()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': DB_PASSWORD
+        'HOST': '',
+        'PORT': '',
+    }
+}
+"""
 
 # static and media files
 STATICFILES_DIRS = (
-    PROJECT_ROOT.child('static'),
     '/var/www/acros/static',
 )
 

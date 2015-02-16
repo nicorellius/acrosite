24b400ff4af55612b691987d1c27b76d300310c6
root <root@acrosite.(none)>
2015-02-16 15:29:29 -0500

    some server rlated changes

--- acros/acros/wsgi.py
+++ acros/acros/wsgi.py
@@ -8,7 +8,16 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import site
+
+site.addsitedir('/home/dev/virtualenv/acrosite/lib/python3.3/site-packages/')
+
+sys.path.append('/home/dev/django/projects/acrosite/acros')
+sys.path.append('/home/dev/django/projects/acrosite/acros/acros')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "acros.settings.production")
 
 from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()

c7cfce9eeb359d0c7832447d668faaa94e996d2d
nicorellius <nicorellius@gmail.com>
2015-02-27 17:14:17 -0800

    added sitemaps application to experimental

--- acros/acros/urls.py
+++ acros/acros/urls.py
@@ -8,12 +8,21 @@ description :   main URLConf for acrosite
 
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
+
+from common.sitemaps import SitePageSitemap, SiteFlatPageSitemap
 
 from django.contrib import admin
 
 from apps.generator.views import GenerateAcrosticFormView
 from apps.generator.views import GenerateAcrosticSuccessView
 from apps.generator.views import RateAcrosticView
+
+
+sitemaps = {
+    'flatpages': SiteFlatPageSitemap,
+    'pages': SitePageSitemap(['index', ]),
+}
 
 
 admin.autodiscover()
@@ -26,10 +35,12 @@ urlpatterns = patterns(
     url(r'^accounts/', include('allauth.urls')),
 
     # main page index
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
 
     # acrostic viewer
     url(r'^generate/$', GenerateAcrosticFormView.as_view(), name='generate_acrostic_form_view'),
     url(r'^generate/acrostic/$', GenerateAcrosticSuccessView.as_view(), name='generate_acrostic_success_view'),
     url(r'^acrostic/rate/$', RateAcrosticView.as_view(), name='rate_acrostic_view'),
+
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 )
